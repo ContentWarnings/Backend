4b3e4095e198143435563bbb36d627cdb7690150
Jeffrey DiVincent <git@divi.sh>
2023-01-29 20:09:24 -0500

    Clean up how env variables are handled in test_main

--- src/test_main.py
+++ src/test_main.py
@@ -22,10 +22,38 @@ def test_env_variables():
     """
     Test for required environment variables.
     """
-    assert os.environ["TMDB_API_KEY"], "Missing env variable: TMDB_API_KEY"
-    assert os.environ["MOVIES_TABLE"], "Missing env variable: MOVIES_TABLE"
-    assert os.environ["CW_TABLE"], "Missing env variable: CW_TABLE"
-    assert os.environ[
-        "USER_VERIFICATION_TABLE"
-    ], "Missing env variable: USER_VERIFICATION_TABLE"
-    assert os.environ["USER_TABLE"], "Missing env variable: USER_TABLE"
+
+    # Env key presence checks:
+    keys = [
+        "TMDB_API_KEY",
+        "MOVIES_TABLE",
+        "CW_TABLE",
+        "USER_VERIFICATION_TABLE",
+        "USER_TABLE",
+        "JWT_SECRET",
+        "JWT_ALGORITHM",
+        "JWT_SUDO_LIFETIME",
+        "JWT_USER_LIFETIME",
+    ]
+    for key in keys:
+        try:
+            assert os.environ[key], f"Missing env variable: {key}"
+        except KeyError:
+            assert False, f"Missing env variable: {key}"
+
+        if "_TABLE" in key:
+            assert (
+                "-dev" not in key or "-prod" not in key
+            ), "Environment missing from _TABLE key."
+
+    # JWT sanity checks
+    assert (
+        os.environ["JWT_ALGORITHM"] == "HS256"
+    ), "Unexpected env variable: Why was the JWT algorithm changed?"
+    assert len(os.environ["JWT_SECRET"]) >= 64, "JWT secret is too weak."
+    assert (
+        int(os.environ["JWT_USER_LIFETIME"]) <= 864000
+    ), "JWT lifetime is too long (User)"
+    assert (
+        int(os.environ["JWT_SUDO_LIFETIME"]) <= 86400
+    ), "JWT lifetime is too long (Admin)"
