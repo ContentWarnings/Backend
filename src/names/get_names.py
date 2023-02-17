1a575654acb88b977e86710756ca5f2771dc50fc
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2023-02-17 12:25:09 -0500

    Add Logging to /names for IP debugging (#55)

--- src/names/get_names.py
+++ src/names/get_names.py
@@ -1,12 +1,16 @@
 from ..cw.ContentWarningNames import ContentWarningNames
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 get_names_router = APIRouter()
 
 
 @get_names_router.get("/names")
-def get_all_cw_names():
+def get_all_cw_names(request: Request):
     """
     Returns an ordered list of CW names/types
     """
+    
+    # TODO: remove, this is just for testing purposes
+    print(request.headers)
+
     return {"cws": [name.value for name in ContentWarningNames]}
