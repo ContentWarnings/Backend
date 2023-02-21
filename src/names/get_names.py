c8b83b6c105f8a259f30dcf6f38449a65e969a80
Adam Fernandes <57380803+Quikks1lver@users.noreply.github.com>
2023-02-20 21:18:46 -0500

    More Sorting in /search (#61)
    * add'l search sorting
    
    * removed logging IPs in /names
    
    * removed old code from /names

--- src/names/get_names.py
+++ src/names/get_names.py
@@ -1,16 +1,13 @@
 from ..cw.ContentWarningNames import ContentWarningNames
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 
 get_names_router = APIRouter()
 
 
 @get_names_router.get("/names")
-def get_all_cw_names(request: Request):
+def get_all_cw_names():
     """
     Returns an ordered list of CW names/types
     """
-    
-    # TODO: remove, this is just for testing purposes
-    print(request.headers)
 
     return {"cws": [name.value for name in ContentWarningNames]}
