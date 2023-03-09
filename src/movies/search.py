fd66835428a8ce084e5a71737a7c47b8c6412293
Jeffrey DiVincent <43482496+jeffreydivi@users.noreply.github.com>
2023-03-09 15:54:48 -0500

    Search enhancements (#72)
    * Search enhancements
    
    * Better pagination
    
    * Bugfixes as per pytest
    
    ---------
    
    Co-authored-by: Jeffrey DiVincent <git@divi.sh>

--- src/movies/search.py
+++ src/movies/search.py
@@ -4,6 +4,7 @@
 # https://fastapi.tiangolo.com/tutorial/query-params/
 # https://stackoverflow.com/questions/37343369/is-there-a-way-to-sort-custom-objects-in-a-custom-way-in-python
 # https://docs.python.org/3/howto/sorting.html
+# https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string#5618893
 
 from ..tmdb.tmdb import TMDB
 from ..tmdb.genres import Genre, GENRE_MAP
@@ -25,11 +26,56 @@ dynamodb_client = boto3.client("dynamodb")
 MOVIES_TABLE = os.environ["MOVIES_TABLE"]
 
 
-def get_trending_movies() -> requests.Response:
+def get_trending_movies(genre=Genre.Disregard, mm_page=1) -> requests.Response:
     """
     Returns trending movies from the last day, in JSON format
     """
-    return TMDB.hit_api("trending/movie/day")
+
+    if genre == Genre.Disregard:
+        if mm_page == 1:
+            return TMDB.hit_api("trending/movie/day").json()
+        else:
+            return {"results": []}
+    else:
+        gid = GENRE_MAP[genre]
+
+        return search_movie(
+            query="", mm_page=mm_page, endpoint="discover/movie", genre_id=gid
+        )
+
+
+def search_movie(
+    query: str, mm_page: int, endpoint: str = "search/movie", genre_id: int = None
+):
+    output = []
+
+    pages_to_sum = 3
+
+    # MovieMentor page -> TMDB page to start at
+    tmdb_start_page = ((mm_page - 1) * pages_to_sum) + 1
+
+    for subpage in range(0, pages_to_sum):
+        tmdb_page = tmdb_start_page + subpage
+
+        # Code reuse: genre filtering pagination and movie pagination is the same.
+        if endpoint == "discover/movie":
+            response = TMDB.hit_api(
+                endpoint, f"&with_genres={genre_id}&page={tmdb_page}"
+            )
+        else:
+            response = TMDB.hit_api(endpoint, f"&query={query}&page={tmdb_page}")
+
+        json_map: dict = json.loads(response.text)
+        for val in json_map["results"]:
+            output.append(val)
+
+        # Do not call additional pages if there are no more pages!
+        if tmdb_page >= json_map["total_pages"]:
+            break
+
+    obj = {"results": output}
+
+    return obj
 
 
 @search_router.get("/search")
@@ -44,18 +90,16 @@ def search(
     """
 
     # retrieve trending movies if query string is null or empty
-    response = (
-        get_trending_movies()
+    json_map: dict = (
+        get_trending_movies(genre, p)
         if q is None or len(q.strip()) == 0
-        else TMDB.hit_api("search/movie", f"&query={q}&page={p}")
+        else search_movie(q, p)
     )
-
-    json_map: dict = json.loads(response.text)
 
     movies_list: List[MovieReduced] = []
 
     # return empty list if no results
-    if "results" not in json_map.keys():
+    if len(json_map.get("results", [])) <= 0:
         # logging
         print("No results for TMDB search.")
         print(json_map)
